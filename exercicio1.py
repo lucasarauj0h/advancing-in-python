5cec81bea1835ac263ee78ba068c24a54ac2d9c7
Lucas Araujo <souza.a@aluno.ufabc.edu.br>
2023-08-25 12:04:17 -0300

    new study 25.08

--- exercicio1.py
+++ exercicio1.py
@@ -14,14 +14,18 @@ Se nada for digitado em nome ou idade:
     exiba "Desculpe, você deixou campos vazios."
 """
 
-nome = input('digite seu nome aq')
-idade = input('agr sua idade')
+nome = input('digite seu nome aq: ')
+idade = input('agr sua idade: ')
 
-if nome != 'null' or idade != 'null':
-    print('seu nome é {nome}')
-    print('seu nome invertido é {nome[::-1]}')
-    print('a primeira letra do seu nome é {nome[0]}')
-    print('a ultima letra do seu nome é {nome[:len(nome)]}')
+if nome and idade:
+    print(f'seu nome é {nome}')
+    print(f'seu nome invertido é {nome[::-1]}')
+    if ' ' in nome:
+        print('seu nome contem espaços')
+    else: 
+        print ('seu nome n tem espaços')
+    print(f'seu nome tem {len(nome)} letras')
+    print(f'a primeira letra do seu nome é {nome[0]}')
+    print(f'a ultima letra do seu nome é {nome[len(nome)-1:len(nome)]}')
 else:
     print('desculpe, você deixou um dos campos vazio')
-    
