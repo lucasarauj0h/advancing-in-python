9030fe387e07abce539e194c1a42a8b2b5e28fbe
Lucas Araujo <souza.a@aluno.ufabc.edu.br>
2023-08-26 20:13:37 -0300

    study 26.08

---
+++ 51_empacotamento_tuplas.py
@@ -0,0 +1,9 @@
+nomes = ['Lucas', 'Diana', 'Maria'] #vamos tirar um dos valores e passar para outro
+
+nome1, nome2, nome3 = nomes
+print(nome1, nome2, nome3)
+print(nomes)
+
+nome4, *_ = ['Lucas', 'Diana', 'Maria']
+print(nome4)
+print(_)
