40d830b0ae217788938d1889f9cbfcc279a5b9fa
Lucas Araujo <souza.a@aluno.ufabc.edu.br>
2023-09-18 23:28:38 -0300

    18.09 study

--- intermediario/lista_de_tarefas.py
+++ intermediario/lista_de_tarefas.py
@@ -8,3 +8,55 @@
 # desfazer = [] -> Refazer ['caminhar', 'fazer café']
 # refazer = todo ['fazer café']
 # refazer = todo ['fazer café', 'caminhar']
+import os
+
+def clear():
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+
+lista = []
+desfazer = []
+refazer = []
+stop = True
+
+while stop:
+
+    print ("Comandos: listar; desfazer; refazer")
+    x = str(input("Digite uma tarefa ou comando: "))
+    
+    if x == '0': 
+        stop = False
+        break
+    
+    clear()
+
+    if x.lower() == 'desfazer':
+        print(len(desfazer))
+        if len(desfazer) < 1:
+            print('Não há mais oque desfazer.')
+        else: 
+            refazer.append(lista.pop())
+            print(refazer)
+    elif x.lower() == 'refazer':
+        if len(refazer) <= 0:
+            print('Não há mais oque refazer.')
+        else: 
+            lista.append(refazer.pop())
+            print(refazer)
+    else: 
+        lista.append(x)
+   
+    desfazer = lista
+    print("items da lista: ")
+    for i in range(len(lista)):
+            print(lista[i])
