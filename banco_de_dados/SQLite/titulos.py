dc503908c5e3faf898a14c3da8485478e176e214
Lucas Araujo <souza.a@aluno.ufabc.edu.br>
2023-10-05 23:14:32 -0300

    new study 05.10 SQL

---
+++ banco_de_dados/SQLite/titulos.py
@@ -0,0 +1,11 @@
+#criar um banco de dados "titulos" contendo nome, ano de lançamento, duração de um filme
+
+import sqlite3
+
+# criando o banco de dados
+
+connection = sqlite3.connect("banco_de_dados/SQLite/title.db") #criando uma base de dados com o nome "title"
+
+# verifica se houve alguma alteração na base de dados 
+
+print(connection.total_changes)
