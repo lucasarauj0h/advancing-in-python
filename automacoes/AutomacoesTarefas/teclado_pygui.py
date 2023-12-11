9228043a87bcc2af3d3d7cc62a8eb34861d08460
Lucas Araujo <souza.a@aluno.ufabc.edu.br>
2023-12-11 17:09:18 -0300

    update pyautogui

--- automacoes/AutomacoesTarefas/teclado_pygui.py
+++ automacoes/AutomacoesTarefas/teclado_pygui.py
@@ -1,17 +1,17 @@
 import pyautogui
 import time
 
-# # Apertando o botão de windows
-# pyautogui.press('winleft')
-# time.sleep(1)
-# # escreva 'calculadora'
-# pyautogui.write('calculadora')
-# time.sleep(1)
-# pyautogui.press('enter')
-# time.sleep(1)
+# Apertando o botão de windows
+pyautogui.press('winleft')
+time.sleep(1)
+# escreva 'calculadora'
+pyautogui.write('calculadora')
+time.sleep(1)
+pyautogui.press('enter')
+time.sleep(1)
 # apertando mais de uma tecla ao mesmo tempo, nesse caso abrirá o gerenciador de tarefas
-# pyautogui.hotkey('ctrl', 'shift', 'esc') 
+pyautogui.hotkey('ctrl', 'shift', 'esc') 
 
-# with pyautogui.hold('winleft'):
-#     pyautogui.press('left')
+with pyautogui.hold('winleft'):
+    pyautogui.press('left')
 
