d908e2a930ee3391fcda61c71ba08821caafa8ae
Lucas Araujo <souza.a@aluno.ufabc.edu.br>
2023-12-21 23:22:20 -0300

    coleta de noticias com bs4

---
+++ automacoes/Noticias/news.py
@@ -0,0 +1,16 @@
+from scraping_sites.site import *
+import os
+from threading import Thread # Permitir multiplas execução de códigos
+import time
+from datetime import datetime
+import sys
+import pickle
+import webbrowser
+from math import ceil 
+from pytimedinput import timedInput
+
+class News:
+    def __init__(self):
+        pass
+    
+self = News()
