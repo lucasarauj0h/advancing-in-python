c1230e6587d2e21ea67a74631c7ce633faf48174
lucas <souza.a@aluno.ufabc.edu.br>
2024-01-25 20:11:31 -0300

    curve roc on credit fraud

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Lucas\OneDrive\Documentos\python 3.0\venv
+set VIRTUAL_ENV=C:\Users\daler\Documents\python-code\advancing-in-python\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
